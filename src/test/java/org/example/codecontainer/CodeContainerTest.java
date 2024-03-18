91f5ba873946af66bc44a9a3f41d1b8612ffdff9
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2024-03-18 11:37:07 +0100

    clean up

--- src/test/java/org/example/codecontainer/CodeContainerTest.java
+++ src/test/java/org/example/codecontainer/CodeContainerTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CodeContainerTest {
 
     @Test
-    void fileNameExtractionHappyFlow() throws RuntimeException {
+    void fileNameExtraction() throws RuntimeException {
         testFileNameExtraction("class HappyFlow{}", "HappyFlow.java");
         testFileNameExtraction("   class HappyFlow{}", "HappyFlow.java");
         testFileNameExtraction("public class HappyFlow{}", "HappyFlow.java");
@@ -17,29 +17,19 @@ class CodeContainerTest {
         testFileNameExtraction("public class    HappyFlow{}", "HappyFlow.java");
     }
 
-    @Test
-    void fileNameExtractionUnhappyFlow() {
-        // TODO: implement
-    }
-
     private void testFileNameExtraction(String content, String expectedFileName) throws RuntimeException {
         CodeContainer co = new CodeContainer(content);
         assertEquals(expectedFileName, co.getFileName());
     }
 
     @Test
-    void packageNameExtractionHappyFlow() throws RuntimeException {
+    void packageNameExtraction() throws RuntimeException {
         testPackageNameExtraction("package happyflow; class C{}", "happyflow");
         testPackageNameExtraction("   package happyflow; class C{}", "happyflow");
         testPackageNameExtraction("package    happyflow; class C{}", "happyflow");
         testPackageNameExtraction("package nl.happyflow; class C{}", "nl.happyflow");
         testPackageNameExtraction("package happyflow   ; class C{}", "happyflow");
         testPackageNameExtraction("class C{}", "");
-    }
-
-    @Test
-    void packageNameExtractionUnhappyFlow() {
-        // TODO: implement
     }
 
     private void testPackageNameExtraction(String content, String expectedPackageName) {
